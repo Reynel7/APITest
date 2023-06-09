964c4ab1317856d76281e04cd2fcc4e89b755d57
reynel7 <reynelsanchez70@gmail.com>
2023-06-09 10:06:10 -0400

    Add Tests

---
+++ TestAPI/TestProject1/GetIdProducts.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Net;
+using TechTalk.SpecFlow;
+
+namespace TestProject1
+{
+    [Binding]
+    public class GetIdProducts
+    {
+        RestClient client = new RestClient("http://demostore.gatling.io/api/product");
+
+        RestRequest request = new RestRequest("posts/{postid}", Method.Get);
+
+        RestResponse response;
+
+        [Given(@"I have an id with value (.*)")]
+        public void GivenIHaveAnIdWithValue(int p0)
+        {
+            request.AddUrlSegment("postid", p0.ToString());
+        }
+
+        [When(@"I send a get request")]
+        public void WhenISendIGetRequest()
+        {
+            response = client.Execute(request);
+        }
+
+        [Then(@"I expected a valid code response")]
+        public void ThenIExpectedAValidCodeResponse()
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+    }
+}
---
+++ TestAPI/TestProject1/features/GetIdProducts.feature
@@ -0,0 +1,12 @@
+﻿Feature: Get Id Products
+
+A short summary of the feature
+
+@positive @smoke @regression @integration 
+Scenario: Get by id 
+	Given I have an id with value 17
+	When I send a get request
+	Then I expected a valid code response
+
+
+
